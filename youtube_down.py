500baf2ef8ccf404711241208057a01a4b0c506b
Ebrahim_ak <y560mia3@gmail.com>
2024-05-26 13:06:03 +0330

    server updated

--- youtube_down.py
+++ youtube_down.py
@@ -6,18 +6,20 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 import requests
 
-
+q1 = 0
 def user_input():
+    global q1
     # url = input('enter youtube link: ')
-    url = 'https://www.youtube.com/watch?v=54PilOtIrAM'
+    url = 'https://www.youtube.com/watch?v=Q2zaO2C2vWk'
     # quality = input('enter quality of video[auto,1080p,720p,480p,240p]: ')
     quality = '480p'
+    q1 = quality
     q_dict = {
-        'auto': '1',
-        '1080p': '2',
-        '720p': '3',
-        '480p': '4',
-        '240p': '5'
+        '720p': 'btn22',
+        '480p': 'btn135',
+        '360p': 'btn18',
+        '240p': 'btn133',
+        '144p': 'btn160'
     }
     return url, q_dict[quality]
 
@@ -26,7 +28,7 @@ url, q = user_input()
 
 
 def create_target_link(url):
-    return url[:19] + 'pp' + url[19:]
+    return url[:19] + 'pi' + url[19:]
 
 
 def get_link(url, q):
@@ -36,13 +38,13 @@ def get_link(url, q):
         '../chromedriver.exe'))  # give your driver path
     driver.get(url)
     driver.maximize_window()
-    origin_page = driver.current_window_handle
-    Wait = WebDriverWait(driver, 10)
+    # origin_page = driver.current_window_handle
+    Wait = WebDriverWait(driver, 15)
     Wait.until(ec.element_to_be_clickable(
-        (By.XPATH, f'//*[@id="mp4"]/table/tbody/tr[{q}]/td[3]/button'))).click()
-    driver.switch_to.window(origin_page)
+        (By.XPATH, f'//*[@id="{q}"]/button'))).click()
+    # driver.switch_to.window(origin_page)
     row_link = Wait.until(ec.element_to_be_clickable(
-        (By.XPATH, '//*[@id="process-result"]/div/a')))
+        (By.XPATH, '//*[@id="btnOk"]/button/a')))
     link = row_link.get_attribute('href')
     driver.close()
     return link
@@ -51,15 +53,16 @@ def get_link(url, q):
 def download_vid(link):
     with requests.get(link, stream=True) as r:
         total = int(r.headers.get('content-length'))
-        print(f'video size == {total}')
+        print(f'video size == {total/(1024*1024)}')
         download = 0
-        with open(f'{q}.mp4', 'wb') as f:
+        with open(f'{q1}.mp4', 'wb') as f:
             print('downloading...')
-            for data in r.iter_content(4096):
+            for data in r.iter_content(1024):
                 download += len(data)
                 downloaded = int(100*download/total)
                 print(f'\r[{'='*downloaded}{' '*(100-downloaded)}]', end='')
                 f.write(data)
+    print('\ndownload completed!!!!')
 
 
 download_vid(get_link(create_target_link(url), q))
